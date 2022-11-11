615c32f0950f715864a2f856b337c5b3939ea8d8
PamelaBorelli <102415224+PamelaBorelli@users.noreply.github.com>
2022-11-10 23:24:14 -0300

    aula_10_11

--- src/componentes/Componente1.jsx
+++ src/componentes/Componente1.jsx
@@ -1,7 +1,31 @@
 import React from "react";
 
-function funct1 (){
-    return <h1>Nosso primeiro componente</h1>
-}
+// function funct1 (){
+//     return <h1>Nosso primeiro componente</h1>
+// }
 
-export default funct1
+// export default funct1
+
+// export default () =>
+// <div>
+ 
+//     <h1>Nosso primeiro componente</h1>
+//     <h1>{(5+6)}</h1>
+
+// </div>
+
+var vf = false
+export default (props) => 
+    <div>
+        <h1>{props.nome}</h1>
+        <h1>{props.cidade}</h1>
+        <h1>{props.endereco}</h1>
+
+        <h1>{props.soma}</h1>
+
+        <p>{Math.random()}</p>
+        <p>{2*8}</p>
+        <p>{Math.pow(2,8)}</p>
+        <p>Vedadeiro ou falso? {vf ?'verdadeiro' : 'Falso'}</p>
+
+    </div>
--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Componente1 from "./componentes/Componente1";
+import {CompA, CompB as B} from "./componentes/DoisComponentes";
+import MultiElementos from "./componentes/MultiElementos";
 
 const element = document.getElementById ('root')
 
 ReactDOM.render (
     <div>
-        <Componente1/>
+        {/* <Componente1 nome = 'Pamela' endereco = 'Rua tal' cidade = 'Franca' soma = {3+4}/>
+        <CompA valor = "Aqui é o componente A"/>
+        <B valor = "Aqui é o componente B"/> */}
+        <MultiElementos/>
     </div>
 , element
 )
