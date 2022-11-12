2ec6aa30c701a67370c395f9da1ae23d6e8623dd
PamelaBorelli <102415224+PamelaBorelli@users.noreply.github.com>
2022-11-11 21:42:43 -0300

    aula_11_11

---
+++ src/componentes/Borelli.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Nome from "./Nome";
+
+
+export default props =>
+<div>
+    <Nome nome = "Pamela " sobrenome = "Borelli"/>
+    <Nome nome = "Joaquim " sobrenome = "Borelli"/>
+    <Nome nome = "Pedro " sobrenome = "Borelli"/>
+
+</div>
--- src/index.js
+++ src/index.js
@@ -3,15 +3,35 @@ import ReactDOM from "react-dom";
 import Componente1 from "./componentes/Componente1";
 import {CompA, CompB as B} from "./componentes/DoisComponentes";
 import MultiElementos from "./componentes/MultiElementos";
+import Borelli from "./componentes/Borelli";
+import Familia from "./componentes/Familia";
+import Nome from "./componentes/Nome";
 
 const element = document.getElementById ('root')
 
 ReactDOM.render (
-    <div>
+    <React.Fragment>
+    
         {/* <Componente1 nome = 'Pamela' endereco = 'Rua tal' cidade = 'Franca' soma = {3+4}/>
         <CompA valor = "Aqui é o componente A"/>
-        <B valor = "Aqui é o componente B"/> */}
-        <MultiElementos/>
-    </div>
+        <B valor = "Aqui é o componente B"/>
+        <MultiElementos/> */}
+
+        <Familia familia = "Familia Borelli">
+            <Nome nome = "Pamela " sobrenome = "Borelli"/>
+            <Nome nome = "Gianluca " sobrenome = "Borelli"/>
+            <Nome nome = "Sylzen " sobrenome = "Borelli"/>
+        </Familia>
+
+        <Familia familia = "Familia Qualquer">
+            <Nome nome = "Pedro " sobrenome = "Qualquer"/>
+            <Nome nome = "José " sobrenome = "Qualquer"/>
+            <Nome nome = "Lúcia " sobrenome = "Qualquer"/>
+        </Familia>
+
+
+        
+
+    </React.Fragment>
 , element
 )
